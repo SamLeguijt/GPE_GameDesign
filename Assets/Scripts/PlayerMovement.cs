dd51ed26aa00d6eb64a4a8ad8f518daaca06dcc0
SamLeguijt <sam.leguijt@gmail.com>
2024-09-04 15:32:59 +0200

    Added: - Temp movement code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private PlayerInput inputController = null;
+    [SerializeField] private float moveSpeed = 3f;
+    private void OnEnable()
     {
-        
+        inputController.MoveLeftInput += OnMoveLeftInputReceivedEvent;
+        inputController.MoveRightInput += OnMoveRightInputReceivedEvent;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        inputController.MoveLeftInput -= OnMoveLeftInputReceivedEvent;
+        inputController.MoveRightInput -= OnMoveRightInputReceivedEvent;
+    }
+
+    private void OnMoveLeftInputReceivedEvent()
+    {
+        transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+    }
+    
+    private void OnMoveRightInputReceivedEvent()
+    {
+        transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
     }
 }
