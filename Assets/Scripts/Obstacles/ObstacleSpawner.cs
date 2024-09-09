04e47b57449072dda61fc3a3dad843de8aaea645
SamLeguijt <sam.leguijt@gmail.com>
2024-09-09 21:05:32 +0200

    Changes: - Made object spawner work with budgets per obstacle color - Spawning objects now get color based on budget list

--- Assets/Scripts/Obstacles/ObstacleSpawner.cs
+++ Assets/Scripts/Obstacles/ObstacleSpawner.cs
@@ -4,29 +4,96 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
+    public const int GREEN_INDEX = 0;
+    public const int BLUE_INDEX = 1;
+    public const int RED_INDEX = 2;
+    public const int PURPLE_INDEX = 3;
+    public const int ORANGE_INDEX = 4;
+
     [Header("References")]
     [SerializeField] private GameObject obstaclePrefab = null;
 
-    [Header("Settings")]
+    [Header("Spawn Settings")]
     [SerializeField] private float spawnDelay = 1.5f;
     [SerializeField] private int objectsPerSpawnTick = 3;
+
+    [field: Header("Object budgets")]
+    [field: SerializeField] public int GreenBudget {get; private set;} = 1;
+    [field: SerializeField] public int BlueBudget { get; private set; } = 0;
+    [field: SerializeField] public int RedBudget { get; private set; } = 0;
+    [field: SerializeField] public int PurpleBudget { get; private set; } = 0;
+    [field: SerializeField] public int OrangeBudget { get; private set; } = 0;
 
     private Vector2[] spawnPositons;
     private List<ColorData> colors = new List<ColorData>();
     private Coroutine spawningRoutine = null;
 
+    private List<int> obstaclesBudgetTable = new List<int>();
+
     private void Start()
     {
         colors = GameManager.Instance.ColorContainer.GetAllColorData();
-     
+
+        ResetObstacleBudget();
         SetupSpawnPositions();
         StartSpawningObstacles();
     }
 
+    public void SetSpawnSettings(float spawnDelay, int objectsPerSpawn)
+    {
+        this.spawnDelay = spawnDelay; 
+        this.objectsPerSpawnTick = objectsPerSpawn;
+    }
+
+    public void SetObjectBudgets(int green, int blue, int red, int purple, int orange)
+    {
+        GreenBudget = green;
+        BlueBudget = blue; 
+        RedBudget = red;
+        PurpleBudget = purple;
+        OrangeBudget = orange;
+    }
+
+    private void ResetObstacleBudget()
+    {
+        obstaclesBudgetTable.Clear();
+
+        if (GreenBudget > 0)
+        {
+            for (int i = 0; i < GreenBudget; i++)
+                obstaclesBudgetTable.Add(GREEN_INDEX);
+        }
+
+        if (BlueBudget > 0)
+        {
+            for (int i = 0; i < BlueBudget; i++)
+                obstaclesBudgetTable.Add(BLUE_INDEX);
+        }
+
+        if (RedBudget > 0)
+        {
+            for (int i = 0; i < RedBudget; i++)
+                obstaclesBudgetTable.Add(RED_INDEX);
+        }
+
+
+        if (PurpleBudget > 0)
+        {
+            for (int i = 0; i < PurpleBudget; i++)
+                obstaclesBudgetTable.Add(PURPLE_INDEX);
+        }
+
+        if (OrangeBudget > 0)
+        {
+            for (int i = 0; i < OrangeBudget; i++)
+                obstaclesBudgetTable.Add(ORANGE_INDEX);
+        }
+    }
+
     private void OnEnable()
     {
-        GameManager.Instance.GameStartedEvent += StartSpawningObstacles;    
-        GameManager.Instance.GameEndedEvent += StopSpawningObstacles;    
+        GameManager.Instance.GameStartedEvent += StartSpawningObstacles;
+        GameManager.Instance.GameEndedEvent += StopSpawningObstacles;
     }
 
     private void OnDisable()
@@ -76,8 +143,11 @@ public class ObstacleSpawner : MonoBehaviour
             {
                 Vector2 spawnPos = GetRandomSpawnPosition();
 
-                Obstacle obstacle = Instantiate(obstaclePrefab, spawnPos, Quaternion.identity).GetComponent<Obstacle>() ;
-                obstacle.Initialize(colors[GetRandomInt(0, colors.Count)]);
+                Obstacle obstacle = Instantiate(obstaclePrefab, spawnPos, Quaternion.identity).GetComponent<Obstacle>();
+
+                int randomColorIndex = GetRandomInt(0, obstaclesBudgetTable.Count);
+                int colorIndex = obstaclesBudgetTable[randomColorIndex];
+                obstacle.Initialize(colors[colorIndex]);
             }
 
             elapsedTime += Time.deltaTime;
